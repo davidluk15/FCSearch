57366546d5f5d79612c5253b3cb59aafc8c5fce9
davidluk15 <lukas.david@edu.fh-joanneum.at>
2018-06-21 15:33:09 +0200

    PlayerController

--- src/at/fh/swenga/project/controller/PlayerController.java
+++ src/at/fh/swenga/project/controller/PlayerController.java
@@ -1,52 +1,44 @@
 package at.fh.swenga.project.controller;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
-import org.fluttercode.datafactory.impl.DataFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import at.fh.swenga.project.dao.ClubRepository;
+
 import at.fh.swenga.project.dao.PlayerRepository;
-import at.fh.swenga.project.model.ClubModel;
 import at.fh.swenga.project.model.PlayerModel;
 
 
-@Controller
+@Controller 
 public class PlayerController {
 
 	@Autowired
 	PlayerRepository playerRepository;
 	
-	@Autowired
-	ClubRepository clubRepository;
-
-	@RequestMapping(value = { "/", "list" })
+	@RequestMapping(value = { "/", "index" })
 	public String index(Model model) {
-		List<PlayerModel> players = playerRepository.findAll();
-		model.addAttribute("players", players);
-		model.addAttribute("count", players.size());
+		
 		return "index";
 	}
 	
-	@RequestMapping(value = { "/getPage" })
-	public String getPage(Pageable page,Model model) {
-		Page<PlayerModel> playersPage = playerRepository.findAll(page);
-		model.addAttribute("playersPage", playersPage);
-		model.addAttribute("players", playersPage.getContent());
-		model.addAttribute("count", playersPage.getTotalElements());
-		return "index";
-	}
 
+	
+	@RequestMapping(value = { "/listPlayers" })
+	public String showAllSeries(Model model) {
+			
+		
+		List<PlayerModel> playerModel = playerRepository.findAll();
+		model.addAttribute("playerModel", playerModel);
+		
+	
+		return "listPlayers";
+	}
+	
+	
 	@RequestMapping(value = { "/find" })
 	public String find(Model model, @RequestParam String searchString, @RequestParam String searchType) {
 		List<PlayerModel> players = null;
@@ -54,31 +46,31 @@ public class PlayerController {
 
 		switch (searchType) {
 		case "query1":
-			players = playerRepository.findByLastName(searchString);
+			//employees = employeeRepository.findAll();
 			break;
 		case "query2":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.findByLastName(searchString);
 			break;
 		case "query3":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.findByFirstName(searchString);
 			break;
 		case "query4":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.findByFirstOrLastName(searchString);
 			break;
 		case "query5":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.findByFirstOrLastNameZwei(searchString);
 			break;
 		case "query6":
-			//employees = employeeRepository.;
+			//count = employeeRepository.countByLastName(searchString);
 			break;
 		case "query7":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.removeByLastName(searchString);
 			break;
 		case "query8":
 			//employees = employeeRepository.;
 			break;
 		case "query9":
-			//employees = employeeRepository.;
+			//employees = employeeRepository.findByLastNameContainingIgnoreCaseOrFirstNameContainingIgnoreCase(searchString,searchString);
 			break;
 		case "query10":
 			//employees = employeeRepository.;
@@ -113,66 +105,45 @@ public class PlayerController {
 		}
 		return "index";
 	}
-
-	@RequestMapping(value = { "/findById" })
-	public String findById(@RequestParam("playerId") PlayerModel e, Model model) {
-		if (e!=null) {
-			List<PlayerModel> players = new ArrayList<PlayerModel>();
-			players.add(e);
-			model.addAttribute("players", players);
-		}
-		return "index";
-	}
-
-
-
-	@RequestMapping("/fillEmployeeList")
-	@Transactional
-	public String fillData(Model model) {
-
-		DataFactory df = new DataFactory();
-		
-		ClubModel club = null;
-		
-		for(int i=0;i<100;i++) {
-			if (i%10==0) {
-				String clubName=df.getBusinessName();
-				club=clubRepository.findByClubName(clubName);
-				if (club==null) {
-					club = new ClubModel(clubName, clubName, clubName, clubName,clubName, clubName,1990 );
-				}
-			}
-			
-			
-			
-			PlayerModel playerModel = new PlayerModel(df.getFirstName(),df.getLastName(), df.getFirstName(), 2, df.getFirstName());
-			playerModel.setClub(club);
-			playerRepository.save(playerModel);
-		}
 	
-		return "forward:list";
+	
+	
+	@RequestMapping(value = {"addPlayer"})
+	public String showAddPlayer(Model model) {
+		
+		return "editPlayer";
 	}
 	
+	@RequestMapping(value = {"editSeries"})
+	public String showEditPlayer(Model model) {
+		
+		return "editPlayer";
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	@RequestMapping("/delete")
-	public String deleteData(Model model, @RequestParam int id) {
+	@RequestMapping("/deletePlayer")
+	public String deletePlayer(Model model, @RequestParam int id) {
 		playerRepository.deleteById(id);
 
-		return "forward:list";
+		return "forward:listPlayers";
+}
+	
+	
+	@RequestMapping(value = { "login" })
+	public String login(Model model) {
+		
+		return "login";
 	}
-
-	@ExceptionHandler(Exception.class)
-	public String handleAllException(Exception ex) {
-
-		return "error";
-
-	}
+	
+	
+	@RequestMapping(value = { "register" })
+	public String register(Model model) {
+		
+		return "register";
+}
+	
+	
+	
+	
+	
+	
 }
