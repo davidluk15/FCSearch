5b12f6c2ea9c09201c543dd2aea53896a24b0fd9
davidluk15 <lukas.david@edu.fh-joanneum.at>
2018-06-21 11:13:56 +0200

    Changes ClubController

--- src/at/fh/swenga/project/dao/PlayerRepository.java
+++ src/at/fh/swenga/project/dao/PlayerRepository.java
@@ -1,13 +1,27 @@
 package at.fh.swenga.project.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import at.fh.swenga.project.model.ClubModel;
 import at.fh.swenga.project.model.PlayerModel;
 
 @Repository
 @Transactional
 public interface PlayerRepository extends JpaRepository<PlayerModel, Integer> {
+	
+	
+	List<PlayerModel>findByFirstName(String firstName);
+	
+	List<PlayerModel>findByLastName(String lastName);
+	
+	List<PlayerModel>findByPosition (String position);
+	
+	List<PlayerModel>findByAvailabelTrainingDays (String availabelTrainingDays);
+	
 	
 }
